64d6dfee7b93773a3519978202d0a1e58c2044a7
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-03-19 15:47:25 -0300

    User entity and resource

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,7 +1,12 @@
 package com.educandoweb.course.entities;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
 	private Long id;
 	private String name;
 	private String email;
@@ -57,7 +62,30 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
